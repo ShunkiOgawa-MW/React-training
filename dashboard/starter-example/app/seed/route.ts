774d0c2e76a855e311457b38bfd9bfb9fb567164
Shunki Ogawa <shunki.ogawa@microworld.co.jp>
2026-05-14 17:16:24 +0900

    feat:第4章~第11章。

--- dashboard/starter-example/app/seed/route.ts
+++ dashboard/starter-example/app/seed/route.ts
@@ -1,9 +1,9 @@
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
-
+ 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
+ 
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`
@@ -14,7 +14,7 @@ async function seedUsers() {
       password TEXT NOT NULL
     );
   `;
-
+ 
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
@@ -25,13 +25,13 @@ async function seedUsers() {
       `;
     }),
   );
-
+ 
   return insertedUsers;
 }
-
+ 
 async function seedInvoices() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
+ 
   await sql`
     CREATE TABLE IF NOT EXISTS invoices (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -41,7 +41,7 @@ async function seedInvoices() {
       date DATE NOT NULL
     );
   `;
-
+ 
   const insertedInvoices = await Promise.all(
     invoices.map(
       (invoice) => sql`
@@ -51,13 +51,13 @@ async function seedInvoices() {
       `,
     ),
   );
-
+ 
   return insertedInvoices;
 }
-
+ 
 async function seedCustomers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
+ 
   await sql`
     CREATE TABLE IF NOT EXISTS customers (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -66,7 +66,7 @@ async function seedCustomers() {
       image_url VARCHAR(255) NOT NULL
     );
   `;
-
+ 
   const insertedCustomers = await Promise.all(
     customers.map(
       (customer) => sql`
@@ -76,10 +76,10 @@ async function seedCustomers() {
       `,
     ),
   );
-
+ 
   return insertedCustomers;
 }
-
+ 
 async function seedRevenue() {
   await sql`
     CREATE TABLE IF NOT EXISTS revenue (
@@ -87,7 +87,7 @@ async function seedRevenue() {
       revenue INT NOT NULL
     );
   `;
-
+ 
   const insertedRevenue = await Promise.all(
     revenue.map(
       (rev) => sql`
@@ -97,10 +97,10 @@ async function seedRevenue() {
       `,
     ),
   );
-
+ 
   return insertedRevenue;
 }
-
+ 
 export async function GET() {
   try {
     const result = await sql.begin((sql) => [
@@ -109,9 +109,10 @@ export async function GET() {
       seedInvoices(),
       seedRevenue(),
     ]);
-
+ 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
 }
+ 
